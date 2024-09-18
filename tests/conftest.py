d6bc1a576268d3234f8193cfdd6b3b56c610226f
deeenes <turei.denes@gmail.com>
2024-09-18 15:04:02 +0200

    test for connection param from yaml is almost ready

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,32 @@
+import os
 import pytest
+import yaml
 
 @pytest.fixture
-def dummy_connection_config():
+def testtmp(tmpdir_factory):
+
+    return tmpdir_factory.mktemp('omnipath-metabo-tests')
+
+@pytest.fixture
+def dummy_con_param():
+
+    return {
+        'user': 'testuser',
+        'password': 'testpw',
+        'host': 'myhost',
+        'port': 5432,
+        'database': 'testdb',
+    }
 
 
+
+@pytest.fixture
+def dummy_connection_config(testtmp, dummy_con_param):
+
+    path = os.path.join(testtmp, 'config.yaml')
+
+    with open(path, 'w') as fp:
+
+        fp.write(yaml.dump(dummy_con_param))
+
+    return path
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,3 +1,7 @@
 from omnipath_metabo.schema import _connection
 
-def test_param_yaml():
+def test_param_yaml(dummy_connection_config, dummy_con_param):
+
+    con = _connection.Connection(dummy_connection_config)
+
+    assert con._param == dummy_con_param
