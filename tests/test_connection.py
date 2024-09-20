15c3da72796d16953a45523127824cd9a5ae7a69
unknown <treehyde1@gmail.com>
2024-09-20 13:54:53 +0200

    test connection parameters from dict

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -5,3 +5,9 @@ def test_param_yaml(dummy_connection_config, dummy_con_param):
     con = _connection.Connection(dummy_connection_config)
 
     assert con._param == dummy_con_param
+
+def test_param_dict(dummy_con_param):
+
+    con = _connection.Connection(dummy_con_param)
+
+    assert con._param == dummy_con_param
