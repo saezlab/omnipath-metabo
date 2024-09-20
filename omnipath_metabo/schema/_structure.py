efe8a6be916c2f81e7d1bf92e35efd442b58a2be
deeenes <turei.denes@gmail.com>
2024-09-20 14:45:46 +0200

    `schema._main`: create schema using engine from connection

--- omnipath_metabo/schema/_structure.py
+++ omnipath_metabo/schema/_structure.py
@@ -1,10 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from ._main import Base
 
-Base = declarative_base()
 
 class Structure(Base):
     __tablename__ = 'structures'
     id = Column(Integer, primary_key=True)
-    mol_name = Column(String)
-    mol_structure = Column('mol_structure', String)
+    name = Column(String)
+    structure = Column('mol_structure', String)
