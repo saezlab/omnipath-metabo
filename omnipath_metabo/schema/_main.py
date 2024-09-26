c5e1ce88ec16622d7d11bb2c6deb34a20a587831
unknown <treehyde1@gmail.com>
2024-09-26 11:15:48 +0200

    Created loader class for inserting into psql

--- omnipath_metabo/schema/_main.py
+++ omnipath_metabo/schema/_main.py
@@ -4,3 +4,20 @@ from ._base import Base
 def create(con):
 
     Base.metadata.create_all(con.engine)
+
+class Loader():
+    #accept scheme and resource. 
+    def __init__(self, scheme, resource, session):
+        self.scheme = scheme
+        self.resource = resource
+        self.session = session
+
+
+    def load(self):
+        for row in self.resource:
+            insert_statement = self.scheme.insert().values(
+                smiles=row['smiles'],
+                accession=row['accession'],
+                inchi=row['inchi']
+                )
+            self.session.execute(insert_statement)
