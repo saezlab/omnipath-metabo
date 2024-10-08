def5dff4b65d85f5416237acae55e595b1893674
deeenes <turei.denes@gmail.com>
2024-10-08 14:43:59 +0200

    custom SQLAlchemy type for RDKit MOL type

--- omnipath_metabo/schema/_structure.py
+++ omnipath_metabo/schema/_structure.py
@@ -1,14 +1,20 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, types
 from ._base import Base
 from pypath.inputs import hmdb, swisslipids
 
+class MolType(types.UserDefinedType):
+    cache_ok = True
+
+    def get_col_spec(self, **kw):
+        return "MOL"
 
 
 class Structure(Base):
     __tablename__ = 'structures'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    structure = Column('mol_structure', String)
+    smiles = Column(String)
+    mol = Column(MolType)
 
 
 class Hmdb():
@@ -20,10 +26,10 @@ class Hmdb():
             yield met[0]
 
 class SwissLipids():
-    scheme = Structure 
+    scheme = Structure
 
     def __iter__(self):
         for met in swisslipids.swisslipids_lipids():
-            yield met['Lipid ID'], met['SMILES (pH7.3)']         
+            yield met['Lipid ID'], met['SMILES (pH7.3)']
 
 #Ramp and LipidMaps
