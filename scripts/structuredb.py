8656bb40a7be7c2bb02be79767f1f8ca486521c3
deeenes <turei.denes@gmail.com>
2024-10-16 13:51:32 +0200

    structure database loading example

---
+++ scripts/structuredb.py
@@ -0,0 +1,15 @@
+from omnipath_metabo.schema import _main
+from omnipath_metabo.schema import _structure
+
+d = {
+    'user': 'metabo',
+    'password': 'metabo123',
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'metabo',
+}
+db = _main.Database(d)
+
+h = _structure.LipidMaps()
+
+db.load(h)
